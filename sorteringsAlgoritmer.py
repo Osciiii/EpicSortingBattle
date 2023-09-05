c35f607cb30f3474f64a78adc2627ede2ce4fc75
Ahmad <ahmm0013@gmail.com>
2023-09-05 10:05:42 +0200

    funktion bubbleSort lavet

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,26 +1,5 @@
 import random, copy
 
-def bogoSort(items):
-    # Kopier den liste, vi har modtaget som parameter, så vi ikke ændrer den originale
-    items = items.copy()
-    isSorted = None # Boolean til markering af, om listen er sorteret
-    attempts = 0 # Tællevariabel til at holde styr på antal af forsøg
-    while not isSorted:
-        attempts += 1
-        if attempts > len(items) * 5000: # Check for at stoppe tendensen mod uendeligt
-            print('Giver op på grund af for mange forsøg ({}) og bruger TimSort'.format(attempts))
-            items.sort()
-            return items
-        random.shuffle(items) # Bland alle elementer helt tilfældigt
-        isSorted = True # Vi går ud fra at listen tilfældigvis er sorteret,
-        # ...og prøver i denne løkke at bevise det modsatte
-        for index in range(len(items)-1):
-            if items[index] > items[index+1]:
-                isSorted = False
-                break # Bryd løkken hvis et eneste element er forkert sorteret
-    print('Sorteret efter {} forsøg'.format(attempts))
-    return items
-
 def insertionSort(items):
     for i in range(1, len(items)):
         key = items[i]
@@ -31,6 +10,15 @@ def insertionSort(items):
         items[q+1] = key
     return items
 
+def bubbleSort(items):
+    for i in range(len(items)):
+        for j in range(i):
+            if items[j] > items[j+1]:
+                temp = items[j]
+                items[j] = items[j+1]
+                items[j+1] = temp
+    return items
+
 
 if __name__ == '__main__':
     l = list(range(0, 5))
@@ -38,7 +26,7 @@ if __name__ == '__main__':
     for i in range(50):
         random.shuffle(lb)
         ## Kald den funktion, du vil teste
-        ls = insertionSort(l)
+        ls = bubbleSort(l)
         ## Kald den funktion, du vil teste
         if ls != l:
             print('Fejl! Algoritmen kan ikke sortere.')
